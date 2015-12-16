1c552a429eebc5c8af130edbc54c6beb8ab49d2c
m1sogi <ryudo@misogi.info>
2015-12-16 12:03:20 +0900

    execute rubocop

---
+++ src/rubocopOutput.ts
@@ -0,0 +1,38 @@
+interface RubocopSummary {
+	offence_count: number;
+	target_file_count: number;
+	inspected_file_count: number;
+}
+
+interface RubocopLocation {
+	line: number;
+	column: number;
+	length: number;
+}
+
+interface RubocopOffense {
+	severity: string;
+	message: string;
+	cop_name: string;
+	corrected: boolean;
+	location: RubocopLocation;
+}
+
+interface RubocopFile {
+	path: string;
+	offenses: Array<RubocopOffense>;
+}
+
+interface RubocopMetadata {
+	rubocop_version: string;
+	rubocop_engine: string;
+	ruby_version: string;
+	ruby_patchlevel: string;
+	ruby_platform: string;
+}
+
+export interface RubocopOutput {
+	metadata: RubocopMetadata;
+	files: Array<RubocopFile>;
+	summary: RubocopSummary;
+}
