cb043bffd2e358ed0b5d99968023eaec5bea9dc8
m1sogi <ryudo@misogi.info>
2015-12-16 13:26:35 +0900

    cleanup code

--- src/rubocop.ts
+++ src/rubocop.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
-import { RubocopOutput } from './rubocopOutput'
+import { RubocopOutput } from './rubocopOutput';
 
 interface RubocopConfig {
 	executePath: string;
@@ -9,7 +9,7 @@ interface RubocopConfig {
 
 'use strict';
 export class Rubocop {
-	private config: RobocopConfig;
+	private config: vscode.WorkspaceConfiguration;
 	private diag: vscode.DiagnosticCollection;
 	private path: string;
 	private command: string;
@@ -17,15 +17,15 @@ export class Rubocop {
 	constructor(diagnostics: vscode.DiagnosticCollection) {
 		this.config = vscode.workspace.getConfiguration('ruby.rubocop');
 		this.diag = diagnostics;
-		this.path = this.config.executePath;
-		if(process.platform === 'win32') {
+		this.path = this.config.get('executePath', '');
+		if (process.platform === 'win32') {
 			this.command = 'rubocop.bat';
 		} else {
 			this.command = 'rubocop';
 		}
 	}
 
-	public execute(document: vscode.TextDocument) {
+	public execute(document: vscode.TextDocument): void {
 		if (!this.path || 0 === this.path.length) {
 			vscode.window.showErrorMessage('execute path is empty! please check ruby.rubocop.executePath config');
 			return;
@@ -59,7 +59,7 @@ export class Rubocop {
 		});
 	}
 
-	private severity(sev: string) {
+	private severity(sev: string): vscode.DiagnosticSeverity {
 		switch (sev) {
 			case 'refactor': return vscode.DiagnosticSeverity.Hint;
 			case 'convention': return vscode.DiagnosticSeverity.Information;
