da4d4f3ea4b9f59766a5b8a6280a347b59497848
Sergey Kintsel <sergey.kintsel@toptal.com>
2016-07-20 11:42:19 +0700

    Add rubocop autocorrect and check for config file existance

---
+++ src/rubocopAutocorrect.ts
@@ -0,0 +1,11 @@
+import { Rubocop } from './rubocop';
+
+export class RubocopAutocorrect extends Rubocop {
+  public get isOnSave(): boolean {
+    return false;
+  }
+
+  protected commandArguments(fileName: string): Array<string> {
+    return super.commandArguments(fileName).concat(['--auto-correct']);
+  }
+}
