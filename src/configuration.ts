9f519ea1a5300fb2d8900909bc7ba0f6b67ce4c1
Pedro Rodrigues <pedro@bbde.org>
2019-02-22 14:42:51 +0000

    Move some methods around to fix some TSLint errors

--- src/configuration.ts
+++ src/configuration.ts
@@ -11,8 +11,31 @@ export interface RubocopConfig {
     useBundler: boolean;
 }
 
-export const onDidChangeConfiguration: (rubocop: Rubocop) => () => void = (rubocop) => {
-    return () => rubocop.config = getConfig();
+const detectBundledRubocop: () => boolean = () => {
+    try {
+        cp.execSync('bundle show rubocop', { cwd: vs.workspace.rootPath });
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+const autodetectExecutePath: (cmd: string) => string = cmd => {
+    const key: string = 'PATH';
+    let paths = process.env[key];
+    if (!paths) {
+        return '';
+    }
+
+    let pathparts = paths.split(path.delimiter);
+    for (let i = 0; i < pathparts.length; i++) {
+        let binpath = path.join(pathparts[i], cmd);
+        if (fs.existsSync(binpath)) {
+            return pathparts[i] + path.sep;
+        }
+    }
+
+    return '';
 };
 
 /**
@@ -49,29 +72,6 @@ export const getConfig: () => RubocopConfig = () => {
     };
 };
 
-const detectBundledRubocop: () => boolean = () => {
-    try {
-        cp.execSync('bundle show rubocop', { cwd: vs.workspace.rootPath });
-        return true;
-    } catch (e) {
-        return false;
-    }
-};
-
-const autodetectExecutePath: (cmd: string) => string = (cmd) => {
-    const key: string = 'PATH';
-    let paths = process.env[key];
-    if (!paths) {
-        return '';
-    }
-
-    let pathparts = paths.split(path.delimiter);
-    for (let i = 0; i < pathparts.length; i++) {
-        let binpath = path.join(pathparts[i], cmd);
-        if (fs.existsSync(binpath)) {
-            return pathparts[i] + path.sep;
-        }
-    }
-
-    return '';
+export const onDidChangeConfiguration: (rubocop: Rubocop) => () => void = rubocop => {
+    return () => (rubocop.config = getConfig());
 };
