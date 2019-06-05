189e37b73a67b3d90838c9927f6146c12b3696b2
estani <git@estani.de>
2019-06-05 13:12:42 +0200

    Usinf relative config file path

--- src/rubocop.ts
+++ src/rubocop.ts
@@ -83,13 +83,21 @@ function getCommandArguments(fileName: string): string[] {
     let commandArguments = ['--stdin', fileName, '--format', 'json', '--force-exclusion'];
     const extensionConfig = getConfig();
     if (extensionConfig.configFilePath !== '') {
-        if (fs.existsSync(extensionConfig.configFilePath)) {
-            const config = ['--config', extensionConfig.configFilePath];
-            commandArguments = commandArguments.concat(config);
-        } else {
-            vscode.window.showWarningMessage(`${extensionConfig.configFilePath} file does not exist. Ignoring...`);
+      
+      let found = [extensionConfig.configFilePath].concat(
+        (vscode.workspace.workspaceFolders || []).map((ws: any) => path.join(ws.uri.path, extensionConfig.configFilePath))
+      ).filter((p: string) => fs.existsSync(p));
+
+      if (found.length == 0) {
+        vscode.window.showWarningMessage(`${extensionConfig.configFilePath} file does not exist. Ignoring...`);
+      } else {
+        if (found.length > 1) {
+          vscode.window.showWarningMessage(`Found multiple files (${found}) will use ${found[0]}`);
         }
-    }
+        const config = ['--config', found[0]];
+        commandArguments = commandArguments.concat(config);
+      }
+  }
 
     return commandArguments;
 }
